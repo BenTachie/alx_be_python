c55e88e6cc1920f8ccdbc19c4d9ebb5262142eb8
BenTachie <tachiebenedict@gmail.com>
2025-09-14 11:20:02 +0200

    feat: add simple interest calculator script- simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,12 @@
+# This script calculates simple interest
+
+# Define variables
+principal = 1000   # initial investment (R1000)
+rate = 0.05        # annual interest rate (5%)
+time = 3           # investment period in years
+
+# Calculate simple interest
+interest = principal * rate * time
+
+# Print result
+print(f"The simple interest is: {interest}")
