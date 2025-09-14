52cd5f0fb77806bcaee59adc95d1a1c9e7ffd5d7
BenTachie <tachiebenedict@gmail.com>
2025-09-14 11:07:01 +0200

    feat: add basic arithmetic exercise-  basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+# This is a simple Python program to demonstrate basic arithmetic operations.
+
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+difference = number2 - number1
+product = number1 * number2
+quotient = number1 / number2
+
+print(f"Addition of " + str(number1) + " and " + str(number2) + " is: ", sum)
+print(f"Subtraction of " + str(number1) + " and " + str(number2) + " is: ", difference)
+print(f"Multiplication of " + str(number1) + " and " + str(number2) + " is: ", product)
+print(f"Division of " + str(number1) + " and " + str(number2) + " is: ", quotient)
+
+    
